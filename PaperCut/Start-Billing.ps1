3fa82ed25b8e9d9cc97a1ebd44cdd8b30959c05e
Patrick McMorran <patrick.mcmorran@yale.edu>
2014-09-19 13:39:22 -0400

    Alot of distractions today, just added filtering to the billing script.

--- PaperCut/Start-Billing.ps1
+++ PaperCut/Start-Billing.ps1
@@ -12,6 +12,7 @@
 
 
 param (
+
     #Papercut Server Variables  
     [Parameter(Mandatory=$TRUE, Position=0, HelpMessage="Enter the URL for the PaperCut Server, ie papercut.website.com")] 
     [string[]] 
@@ -39,88 +40,104 @@ param (
     $BannerOraclePassword,
 
     #SFAS Batch File Variables
-    [Parameter(Mandatory=$TRUE, Position=6, HelpMessage="Enter the User ID given by SFAS")] 
+    [Parameter(Mandatory=$TRUE, Position=6, HelpMessage="Enter the Unique ID for this SFAS Billing")] 
+    [string] 
+    $SFASBatchBillingID,
+
+    [Parameter(Mandatory=$TRUE, Position=7, HelpMessage="Enter the User ID given by SFAS")] 
     [string] 
     $SFASBatchUserID,
 
-    [Parameter(Mandatory=$TRUE, Position=7, HelpMessage="Enter the Detail Code given by SFAS")] 
+    [Parameter(Mandatory=$TRUE, Position=8, HelpMessage="Enter the Detail Code given by SFAS")] 
     [string] 
     $SFASBatchDetailCode,
 
-    [Parameter(Mandatory=$TRUE, Position=8, HelpMessage="Enter the file prefix required by SFAS")] 
+    [Parameter(Mandatory=$TRUE, Position=9, HelpMessage="Enter the file prefix required by SFAS")] 
     [string] 
     $SFASBatchFilePrefix,
 
     #SFAS Batch SFTP Server Variables
-    [Parameter(Mandatory=$TRUE, Position=9, HelpMessage="Enter the User ID for the SFAS SFTP Server")] 
+    [Parameter(Mandatory=$TRUE, Position=10, HelpMessage="Enter the User ID for the SFAS SFTP Server")] 
     [string] 
     $SFTPUser,
 
-    [Parameter(Mandatory=$TRUE, Position=10, HelpMessage="Enter the password for the SFTP User")] 
+    [Parameter(Mandatory=$TRUE, Position=11, HelpMessage="Enter the password for the SFTP User")] 
     [string] 
     $SFTPPassword,
 
-    [Parameter(Mandatory=$TRUE, Position=11, HelpMessage="Enter the file path for the SFTP Key")] 
+    [Parameter(Mandatory=$TRUE, Position=12, HelpMessage="Enter the file path for the SFTP Key")] 
     [string] 
     $SFTPKeyPath,
 
-    [Parameter(Mandatory=$TRUE, Position=12, HelpMessage="Enter the URL for the SFTP SFAS Server")] 
+    [Parameter(Mandatory=$TRUE, Position=13, HelpMessage="Enter the URL for the SFTP SFAS Server")] 
     [string] 
     $SFTPServerPath,
 
-    [Parameter(Mandatory=$TRUE, Position=13, HelpMessage="Enter the SFTP Port for the SFTP Server")] 
+    [Parameter(Mandatory=$TRUE, Position=14, HelpMessage="Enter the SFTP Port for the SFTP Server")] 
     [string] 
     $SFTPPortNumber,
 
-    [Parameter(Mandatory=$TRUE, Position=14, HelpMessage="Enter the SSH Host Key Fingerprint of the SFTP Server")] 
+    [Parameter(Mandatory=$TRUE, Position=15, HelpMessage="Enter the SSH Host Key Fingerprint of the SFTP Server")] 
     [string] 
     $SSHHostKeyFingerprint,
 
-    [Parameter(Mandatory=$TRUE, Position=15, HelpMessage="Enter the remote directory used to store the SFAS Billing")] 
+    [Parameter(Mandatory=$TRUE, Position=16, HelpMessage="Enter the remote directory used to store the SFAS Billing")] 
     [string] 
     $RemoteDirectory,
 
-    [Parameter(Mandatory=$TRUE, Position=16, HelpMessage="Enter the FTP protocol used by SFAS SFTP Server")] 
+    [Parameter(Mandatory=$TRUE, Position=17, HelpMessage="Enter the FTP protocol used by SFAS SFTP Server")] 
     [string] 
     $FileProtocol,
 
-    [Parameter(Mandatory=$TRUE, Position=17, HelpMessage="Enter the file path to WinSCP")] 
+    [Parameter(Mandatory=$TRUE, Position=18, HelpMessage="Enter the file path to WinSCP")] 
     [string] 
     $WinSCPPath,
 
     #SMTP E-mail Variables
-    [Parameter(Mandatory=$TRUE, Position=18, HelpMessage="Enter the URL for the Local SMTP Server")] 
+    [Parameter(Mandatory=$TRUE, Position=19, HelpMessage="Enter the URL for the Local SMTP Server")] 
     [string] 
     $SMTPServer,
 
-    [Parameter(Mandatory=$TRUE, Position=19, HelpMessage="Enter the SMTP Port for the SMTP Server")] 
+    [Parameter(Mandatory=$TRUE, Position=20, HelpMessage="Enter the SMTP Port for the SMTP Server")] 
     [string] 
     $SMTPPort,
 
-    [Parameter(Mandatory=$TRUE, Position=20, HelpMessage="Enter whether SSL is enabled on the SMTP Server")] 
+    [Parameter(Mandatory=$TRUE, Position=21, HelpMessage="Enter whether SSL is enabled on the SMTP Server")] 
     [string] 
     $SSLEnabled,
 
-    [Parameter(Mandatory=$TRUE, Position=21, HelpMessage="Enter the User ID for the SMTP Server")] 
+    [Parameter(Mandatory=$TRUE, Position=22, HelpMessage="Enter the User ID for the SMTP Server")] 
     [string] 
     $SMTPUser,
 
-    [Parameter(Mandatory=$TRUE, Position=22, HelpMessage="Enter the password for the previous User ID")] 
+    [Parameter(Mandatory=$TRUE, Position=23, HelpMessage="Enter the password for the previous User ID")] 
     [string] 
     $SMTPPassword,
 
-    [Parameter(Mandatory=$TRUE, Position=23, HelpMessage="Enter the email address the e-mail should appear from")] 
+    [Parameter(Mandatory=$TRUE, Position=24, HelpMessage="Enter the email address the e-mail should appear from")] 
     [string] 
     $EmailFrom,
 
-    [Parameter(Mandatory=$TRUE, Position=24, HelpMessage="Enter the email address to send the summary to")] 
+    [Parameter(Mandatory=$TRUE, Position=25, HelpMessage="Enter the email address to send the summary to")] 
     [string] 
     $EmailTo,
 
-    [Parameter(Mandatory=$TRUE, Position=25, HelpMessage="Enable or Disable the e-mail summary of Billing")] 
+    [Parameter(Mandatory=$TRUE, Position=26, HelpMessage="Enable or Disable the e-mail summary of Billing")] 
     [string] 
-    $SendBillingSummary
+    $SendBillingSummary,
+
+    #Active Directory Parameters
+    [Parameter(Mandatory=$False, Position=27, HelpMessage="Enable or Disable the e-mail summary of Billing")] 
+    [string[]] 
+    $ADWhiteList,
+
+    [Parameter(Mandatory=$False, Position=28, HelpMessage="Enable or Disable the e-mail summary of Billing")] 
+    [string[]] 
+    $ADBlackList
 )
+
+
+#Test to make sure Billing ID is valid:$SFASBatchBillingID
 
 #Load the XML RPC Library 
 $RPCAssemblyPath = Resolve-Path -Path CookComputing.XmlRpcV2.dll
@@ -146,8 +163,40 @@ if($true -eq $FetchUsersSuccess){
     break
 }
 
-#Test results by writing out the list of users.
-Echo "List users"
-foreach ($user in $PaperCutServer.GetPapercutUsers()){
-    echo "$user"
+#Pull Master List of PaperCutUsers
+$PaperCutUsers = $PaperCutServer.GetPapercutUsers();
+
+$ActiveDirectoryBillableUsers
+$BillableUsers
+
+if($)
+#Enumerate all the Whitelisted Billable Users
+foreach($UserGroup in $ADWhiteList){
+    $ActiveDirectoryBillableUsers += Get-ADGroupMember $UserGroup -Recursive | select name
 }
+
+#Filtering out Users not on Whitelist
+foreach($User in $ActiveDirectoryBillableUsers){
+    if($PaperCutUsers.Contains($User)){
+        $BillableUsers += $User
+    }
+}
+
+#Clear PaperCut List
+$PaperCutUsers.Clear()
+#Clear AD User List
+$ActiveDirectoryBillableUsers.Clear()
+
+#Enumerate all users not to be billed
+foreach($UserGroup in $ADBlackList){
+    $ActiveDirectoryBillableUsers += Get-ADGroupMember $UserGroup -Recursive | select name
+}
+
+#Filtering out Users on BlackList
+foreach($User in $ActiveDirectoryBillableUsers){
+    if($BillableUsers.Contains($User)){
+        $BillableUsers.Remove($User)
+    }
+}
+
+
