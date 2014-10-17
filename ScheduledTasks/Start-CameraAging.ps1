1b82cc9cea6be563a2a489b4dcd938d4753c0ec7
Patrick McMorran <patrick.mcmorran@yale.edu>
2014-10-17 17:27:32 -0400

    Started Visio Document for how script works at a resource level, scaffolded a script for the Camera Server.

---
+++ ScheduledTasks/Start-CameraAging.ps1
@@ -0,0 +1,17 @@
+﻿<#
+.Synopsis
+This deletes, renames, and moves camera image files
+.DESCRIPTION
+This is a script that deletes the "Aged Out" Files, Renames Folders to Age out files, and the Moves new files to the new folder.
+.EXAMPLE
+Start-CameraAging.ps1 -RootFolder "h:\ftp" -Days 6
+.LINK
+mailto:patrick.mcmorran@yale.edu
+#>
+Param(
+    [Parameter(Mandatory=$true)]
+    [string[]]$RootFolder,
+    [Parameter(Mandatory=$true)]
+    [int]$Days
+)
+
