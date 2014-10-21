aec74ae449d4db3285706935a88af783c5eaf93d
Patrick McMorran <patrick.mcmorran@yale.edu>
2014-10-21 16:39:12 -0400

    Completed the Camera aging script, which should be ready to run. Testing awaits. Also updated the SFAS Billing diagram yesterday, which I forgot to save.

--- ScheduledTasks/Start-CameraAging.ps1
+++ ScheduledTasks/Start-CameraAging.ps1
@@ -10,8 +10,69 @@ mailto:patrick.mcmorran@yale.edu
 #>
 Param(
     [Parameter(Mandatory=$true)]
-    [string[]]$RootFolder,
+    [string]$RootFolder,
     [Parameter(Mandatory=$true)]
     [int]$Days
 )
 
+$ScriptBlock = {
+        $Disks = Invoke-Command -ComputerName "arch-pc-$using:i" { Get-WmiObject -Class Win32_LogicalDisk | where {$_.DriveType -eq 3} |select DriveType,DeviceID,Size }
+        if( $Disks.Count -eq 1 ){
+            Add-Content D:\Missing.txt "`nArch-PC-$using:i"
+        }elseif ($Disks -eq $null){
+            Add-Content D:\Offline.txt "`nArch-PC-$using:i"
+        }else{
+            Add-Content D:\Online.txt "`nArch-PC-$using:i"
+        }
+    }
+
+
+Set-Location -Path $RootFolder
+
+$Folders = Get-ChildItem -Directory
+
+ForEach($Folder in $Folders){
+#Create a Job for each specific Folder
+    $ScriptBlock = {
+    Set-Location -Path $using:RootFolder
+    Set-Location -Path $using:Folder
+
+        #Validates Structure of Folder
+        for($i = 1; $i -le $using:Days; $i++){
+            $DirectoryPath = [string]::Concat($i,"dayold")
+            echo $DirectoryPath
+            if($false -eq (Test-Path $DirectoryPath)){
+                #Test to ensure enough folders are made for the specified number of days. If not Create the folder
+                New-Item -ItemType directory -Path $DirectoryPath
+            }
+        }#End Folder Validation
+        
+        #Remove the Oldest Folder Acceptable
+        $OldestFolderPath = [string]::Concat($using:Days,"dayold")
+        Remove-Item -Path $OldestFolderPath
+
+        #Migrate Folders
+         for($i = $using:Days; $i -gt 1; $i--){
+            $NewName = [string]::Concat($i,"dayold")
+            $NewPath = [string]::Concat($NewName)            
+            $OldNum = $i - 1
+            $OldPath = [string]::Concat($OldNum,"dayold")
+            if($false -eq (Test-Path $NewPath)){
+                #Test to ensure the New Path does not yet exist
+                Rename-Item $OldPath $NewName
+            }
+        }#End Folder Migration
+
+        #Create the Last Folder (Day 1)
+        $Day1Directory = "1dayold"
+        New-Item -ItemType directory -Path $Day1Directory #Find a way to filter everything to within 24hours?
+        $NewFiles = Get-ChildItem -File  | Where-Object { $_.LastWriteTime -le [DateTime] (get-date).AddDays(-1) }
+        Foreach($File in $NewFiles){
+            Move-Item -Path $File -Destination $Day1Directory
+            #Copy File to $Day1 Day1 Directory
+        }
+        #End Creation of Day 1 Folder
+    }
+
+    Start-Job -ScriptBlock $ScriptBlock   
+}
