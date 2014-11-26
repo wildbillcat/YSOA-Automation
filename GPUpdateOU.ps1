be7bc95bc885479cdd1aa6746aad10a4e7f8ec58
Patrick McMorran <patrick.mcmorran@yale.edu>
2014-11-26 11:55:44 -0500

    Updated the GP Update Script to allow for waiting on job completion, thus giving an indication as to how progress was being made so users wouldn't end their session before all jobs complete

---
+++ GPUpdateOU.ps1
@@ -0,0 +1,8 @@
+﻿$PCs = Get-ADComputer -SearchBase 'OU=Lab Computers,OU=Architecture,DC=yu,DC=yale,DC=edu' -Filter '*' | select -ExpandProperty name
+$Jobs = @()
+foreach($PC in $PCs){
+    $Jobs += Invoke-Command -AsJob -ComputerName $PC {gpupdate /force}
+    "Started GP Update on $PC"
+}
+"Waiting for Jobs to Complete"
+Wait-Job -Job $Jobs
