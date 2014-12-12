b75b2d5b88853c7d172da0af1f41d4f9a3e2208d
Patrick McMorran <patrick.mcmorran@yale.edu>
2014-12-12 11:58:23 -0500

    Created a script to delete user Profiles.

---
+++ ComputerRefresh/DeleteUserProfiles.ps1
@@ -0,0 +1,10 @@
+﻿#Fetch List of user profiles
+$Profiles = Get-CimInstance win32_userprofile 
+
+foreach($Profile in $Profiles){
+    $ProfilePath = $Profile.LocalPath
+    if($ProfilePath.StartsWith("C:\Users\") -and -Not ($ProfilePath.EndsWith("vcg5") -or $ProfilePath.EndsWith("pem4"))){
+        "Delete: $ProfilePath"
+        $Profile | Remove-CimInstance
+    }
+}
