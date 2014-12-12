bfe92c73c42fd8122dbd8155b6162b4a8e51c913
Patrick McMorran <patrick.mcmorran@yale.edu>
2014-12-12 12:30:01 -0500

    Updated the Delete User Profile to clean up the Users Folder Completely.

--- ComputerRefresh/DeleteUserProfiles.ps1
+++ ComputerRefresh/DeleteUserProfiles.ps1
@@ -4,7 +4,17 @@ $Profiles = Get-CimInstance win32_userprofile
 foreach($Profile in $Profiles){
     $ProfilePath = $Profile.LocalPath
     if($ProfilePath.StartsWith("C:\Users\") -and -Not ($ProfilePath.EndsWith("vcg5") -or $ProfilePath.EndsWith("pem4"))){
-        "Delete: $ProfilePath"
+        "Delete Profile: $ProfilePath"
         $Profile | Remove-CimInstance
     }
+}
+
+$Folders = Get-Item -Path "C:\Users\*"
+
+ForEach($Folder in $Folders){
+    if(-Not (($Folder.Name -eq "vcg5") -or ($Folder.Name -eq "pem4") -or $Folder.Name -eq "Public")){
+        "Delete Folder: $Folder"
+        $Folder | Remove-Item -Force -Recurse
+    }
+    
 }
