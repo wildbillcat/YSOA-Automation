5a6adf4fe254d69dc60d03331e5812272f564c80
Patrick McMorran <patrick.mcmorran@yale.edu>
2015-01-13 12:02:48 -0500

    Deleted redunant script

--- SemesterLockout/GPUpdate6thFloor.ps1
+++
@@ -1,2 +0,0 @@
-﻿$Batch =  Get-ADComputer -Filter * -Properties Name -SearchBase "OU=6th Floor Lab,OU=Teaching Labs,OU=Lab Computers,OU=Architecture,DC=yu,DC=yale,DC=edu" | select name
-Invoke-Command -ComputerName $Batch.name { gpupdate /force}
