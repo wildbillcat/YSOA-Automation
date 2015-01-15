f001596323da100fe8916752994b6c648e7867ba
Patrick McMorran <patrick.mcmorran@yale.edu>
2015-01-15 10:44:22 -0500

    Created a script to delegate all my GPOs to SOA Admins to ensure no permission issues are had.

---
+++ AD Administration/Delegate-PolicyPermissions.ps1
@@ -0,0 +1,5 @@
+﻿$MyGPOs = Get-GPO -All | Where-Object {$_.Owner -eq "$env:USERDOMAIN\$env:USERNAME"}
+
+ForEach($Policy in $MyGPOs){
+   $Policy | Set-GPPermissions -TargetName "SOA Admins" -TargetType Group -PermissionLevel GpoEditDeleteModifySecurity -Replace
+}
